7dbfb58039b74c2ad72dd7e9c80d42e531d212b3
Rajib Singh <rajibsingh@gmail.com>
2015-11-22 02:29:56 -0500

    added seed data

--- mysql/folder_document.sql
+++ mysql/folder_document.sql
@@ -1,48 +1,28 @@
-CREATE TABLE family (
+CREATE TABLE folder (
     id MEDIUMINT NOT NULL AUTO_INCREMENT,
     name CHAR(30) NOT NULL,
-    PRIMARY KEY (id)
-) ENGINE=INNODB;
-
-CREATE TABLE person (
-    id MEDIUMINT NOT NULL AUTO_INCREMENT,
-    name CHAR(30) NOT NULL,
-    family_id MEDIUMINT,
+    parent_id MEDIUMINT,
     PRIMARY KEY (id),
-    FOREIGN KEY (family_id)
-        REFERENCES family(id)
+    FOREIGN KEY (parent_id)
+        REFERENCES folder(id)
         ON DELETE CASCADE
 ) ENGINE=INNODB;
 
-CREATE TABLE vehicle (
+CREATE TABLE file (
     id MEDIUMINT NOT NULL AUTO_INCREMENT,
     name CHAR(30) NOT NULL,
-    family_id MEDIUMINT,
+    parent_id MEDIUMINT NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (family_id)
-        REFERENCES family(id)
+    FOREIGN KEY (parent_id)
+        REFERENCES folder(id)
         ON DELETE CASCADE
 ) ENGINE=INNODB;
 
-CREATE TABLE vehicle_person (
-    person_id MEDIUMINT,
-    vehicle_id MEDIUMINT,
-    PRIMARY KEY (person_id, vehicle_id),
-    FOREIGN KEY (person_id)
-        REFERENCES person(id)
-        ON DELETE CASCADE,
-    FOREIGN KEY (vehicle_id)
-        REFERENCES vehicle(id)
-        ON DELETE CASCADE
-);
+insert into folder (name) values ("/");
+insert into folder (name, parent_id) values ("home", 1);
+insert into folder (name, parent_id) values ("raj", 2);
+insert into folder (name, parent_id) values ("guest", 2);
 
-insert into family (name) values ('singh');
-insert into person (family_id, name) values (1, 'raj');
-insert into person (family_id, name) values (1, 'ravi');
-insert into vehicle (family_id, name) values (1, 'porsche');
-insert into vehicle (family_id, name) values (1, 'little tykes');
-insert into vehicle_person (person_id, vehicle_id) values (1, 1);
-insert into vehicle_person (person_id, vehicle_id) values (1, 2);
-insert into vehicle_person (person_id, vehicle_id) values (2, 2);
+
 
 select * from
